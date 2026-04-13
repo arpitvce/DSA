6e05cf55c32fea16c3e56c4cb7caff05970bd4c9
Arpit <lengtharc@gmail.com>
2026-04-13 23:47:40 +0000

    Fixed The Synatx Error in BASH command

--- gitpush.sh
+++ gitpush.sh
@@ -4,6 +4,11 @@ if [ -z "$1" ]; then
    echo "Commit Message required"
    exit 1
 fi
+
+if [ -z "$(git status --porcelain)" ]; then
+	echo "Git Working tree clean"
+	exit 
+fi
 echo "adding changes....."
 git add .
 echo "committing change......"
